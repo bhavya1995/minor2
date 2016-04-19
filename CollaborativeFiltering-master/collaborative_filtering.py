ee0494d73dbbe825b5807e6cb4166973a32b4918
Bhavya Gupta <bhavya1995@gmail.com>
2016-04-19 17:08:55 +0530

    cosine function

--- CollaborativeFiltering-master/collaborative_filtering.py
+++ CollaborativeFiltering-master/collaborative_filtering.py
@@ -7,6 +7,7 @@ import csv
 from math import sqrt
 import pymysql
 import pprint
+from scipy import spatial
 
 pp = pprint.PrettyPrinter(indent=4)
 
@@ -43,14 +44,13 @@ def pearson_correlation(person1,person2):
 
 	# To get both rated items
 	both_rated = {}
-	if (str(person1) == 8 and str(person2) == 11676):
-		print ("aajfkfbkwffkakfkjbka,dfbkffnakfnfankf")
 	for item in dataset[person1]:
 		if item in dataset[person2]:
 			both_rated[item] = 1
-	# pp.pprint(both_rated)
 
-	number_of_ratings = len(both_rated)		
+	number_of_ratings = len(both_rated)
+	# if (number_of_ratings > 1):
+	# 	print (person1 + " " + person2)		
 	
 	# Checking for number of ratings in common
 	if number_of_ratings == 0:
@@ -70,6 +70,10 @@ def pearson_correlation(person1,person2):
 	# Calculate the pearson score
 	numerator_value = product_sum_of_both_users - (person1_preferences_sum*person2_preferences_sum/number_of_ratings)
 	denominator_value = sqrt((person1_square_preferences_sum - pow(person1_preferences_sum,2)/number_of_ratings) * (person2_square_preferences_sum -pow(person2_preferences_sum,2)/number_of_ratings))
+	# if (str(person1) == "8" and str(person2) == "11676"):
+		# print ("aajfkfbkwffkakfkjbka,dfbkffnakfnfankf")
+		# pp.pprint(both_rated)
+		# print (str(numerator_value) + "adad" + str(denominator_value))
 	if denominator_value == 0:
 		return 0
 	else:
@@ -83,7 +87,7 @@ def most_similar_users(person,number_of_users):
 	# Sort the similar persons so that highest scores person will appear at the first
 	scores.sort()
 	scores.reverse()
-	return scores[0:number_of_users]
+	return scores[0:100]
 
 def user_recommendations(person):
 
@@ -124,6 +128,55 @@ def user_recommendations(person):
 	recommendataions_list = [recommend_item for score,recommend_item in rankings]
 	return recommendataions_list
 
+def cosineSimilarity(person):
+	finalArray = set()
+	for d in dataset:
+		if (d == person):
+			continue
+		dataset1 = []
+		dataset2 = []
+		for myself in dataset[person]:
+			dataset1.append(dataset[person][myself])
+		for otherRating in dataset[d]:
+			dataset2.append(dataset[d][otherRating])
+		print (dataset1)
+		print (dataset2)
+		if (len(dataset1) < len(dataset2)):
+			counter = len(dataset2) - len(dataset1)
+			while (counter > 0):
+				dataset1.append(0)
+				counter -= 1
+		if (len(dataset1) > len(dataset2)):
+			counter = len(dataset1) - len(dataset2)
+			while (counter > 0):
+				dataset2.append(0)
+				counter -= 1
+
+		result = 1 - spatial.distance.cosine(dataset1, dataset2)
+		if (result >= 0.995):
+			for otherRating in dataset[d]:
+				finalArray.add(otherRating)
+		resultArray = []
+		for setItem in finalArray:
+			for myself in dataset[person]:
+				flag = 0
+				if (setItem == dataset[person]):
+					flag = 1
+					break
+				if (flag == 1):
+					break
+			if (flag == 0):
+				resultArray.append(setItem)
+
+ 
+	print(resultArray)
+	print ("\n\n\n")
+	print (len(resultArray))
+
+def createMatrix():
+	#Matrix rows users and columns that are books
+	# Cell is ranking user Vs Book and 0 if not present
+	
 
 def import_dataset():
 	conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='bhavya', db='minor2',autocommit=True)
@@ -179,7 +232,7 @@ def import_dataset():
 		else:
 			dataset[str(u[0])][u[1]] = u[2]
 			userid = u[0]
-	pp.pprint(dataset)
+	# pp.pprint(dataset)
 	# cur.execute("SELECT * FROM user_details LIMIT 100")
 	# user_details_data = cur.fetchall()
 	# # print(cur.fetchone())
@@ -200,4 +253,17 @@ def import_dataset():
 
 # pp.pprint (dataset)
 import_dataset()
-print(user_recommendations('8'))
+
+# for other in dataset:	
+# 	temp = similarity_score("201641", other)
+# 	if (temp != 0):
+# 		print (other + " " + str(temp))
+# pp.pprint (most_similar_users("201641", 11962))
+# abc = user_recommendations("201641")
+# print (abc)
+# if (len(abc) == 0):
+# 	pass
+# else:
+# 	# print (other + "\n")
+# 	print(user_recommendations(other))
+cosineSimilarity("8")
